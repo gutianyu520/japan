88ce80d26cb1f72bd9c5095c9ad5824473363add
gutianyu520 <965238929@qq.com>
2019-03-02 17:28:03 +0900

    second commit

--- japan-site-lette/src/main/java/japan/site/listener/LocalContextLoaderListener.java
+++ japan-site-lette/src/main/java/japan/site/listener/LocalContextLoaderListener.java
@@ -1,7 +1,24 @@
 package japan.site.listener;
 
+import javax.servlet.ServletContextEvent;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.context.ContextLoaderListener;
 
 public class LocalContextLoaderListener extends ContextLoaderListener {
+	private static final Logger logger = LoggerFactory.getLogger(LocalContextLoaderListener.class);
+	@Override
+	public void contextInitialized(ServletContextEvent event) {
+		logger.info("自定义监听器正在进行初始化。。。");
+		// TODO Auto-generated method stub
+		super.contextInitialized(event);
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent event) {
+		// TODO Auto-generated method stub
+		super.contextDestroyed(event);
+	}
 
 }
