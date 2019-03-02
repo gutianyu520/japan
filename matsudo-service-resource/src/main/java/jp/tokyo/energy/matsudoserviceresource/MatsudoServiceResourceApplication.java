49c0df66c3b1b7a4ede89fce87a3f761d072c87d
gutianyu520 <965238929@qq.com>
2019-03-02 21:58:04 +0900

    idea first comment

---
+++ matsudo-service-resource/src/main/java/jp/tokyo/energy/matsudoserviceresource/MatsudoServiceResourceApplication.java
@@ -0,0 +1,13 @@
+package jp.tokyo.energy.matsudoserviceresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatsudoServiceResourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatsudoServiceResourceApplication.class, args);
+    }
+
+}
